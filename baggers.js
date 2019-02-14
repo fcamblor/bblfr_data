07cf25d6db221cb8b910d6cebc3ed7250956842d
Frédéric Camblor <fcamblor@gmail.com>
2019-02-14 07:56:50 +0100

    Added fcamblor's talk on Date & Time Back to Basics

--- baggers.js
+++ baggers.js
@@ -11356,6 +11356,13 @@ var data = {
           tags: ['Typescript', 'Typings', 'Transpiler', 'Languages'],
           lang: ['fr'],
         },
+        {
+          title: 'Back to Basics : Ne perdez plus votre Temps avec les Dates',
+          abstract:
+            "<p><strong>La gestion du Temps</strong> sur une application client/serveur peut sembler simple de prime abord, mais se révélera beaucoup plus complexe au fur et à mesure des usages : timezones multiples, heures récurrentes, heure d'été/d'hiver, traitements cron, leap seconds sont des exemples parmi tant d'autres.<br>Saviez-vous même que la Commission Européenne envisage d'abolir le passage en heure d'hiver ? <strong>Quels impacts cela aurait sur nos applications ?</strong></p><p><strong>Mon objectif</strong> : qu'à partir de lundi prochain, vous vous posiez les bonnes questions à chaque fois que vous travaillerez sur une date ou une heure dans vos applications.</p><p>Pour cela, <strong>reprenons les bases du Temps</strong> en informatique : composantes d'une date, norme ISO 8601, Timezones et IANA.</p><p>Une fois ces bases posées, nous verrons, au travers d'un certain nombre de <strong>cas d'utilisation issus de la vraie vie</strong>, les bonnes questions qu'il convient de se poser pour mettre le doigt sur les complexités d'implémentation et éviter de tomber dans une faille spatio-temporelle lors du prochain changement d'heure.</p><p>Ce talk est accessible à tous et ne se focalisera pas sur un langage / une API en particulier : <strong>les concepts prévaudront sur le code</strong>.</p>",
+          tags: ['Back to basics', 'Date', 'Time', 'Timestamps', 'Timezones', 'IANA', 'Bonnes pratiques', 'UTC', 'Temps Atomique International', 'NTP'],
+          lang: ['fr'],
+        },
       ],
       cities: ['Bordeaux'],
       contacts: {
